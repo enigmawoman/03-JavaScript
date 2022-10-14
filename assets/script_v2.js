e57140d956b41ffa69e3b73a8e9d28509d3a0166
enigmawoman <112570078+enigmawoman@users.noreply.github.com>
2022-10-14 19:06:01 +0100

    finished JS code

--- assets/script_v2.js
+++ assets/script_v2.js
@@ -37,39 +37,58 @@ function generatePassword() {
   userOptions.specialChar = window.confirm("Would you like to include special characters?");
   userOptions.numbers = window.confirm("Would you like to include numbers?");
 
-var charList = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", ".", "`", "~", "|", "<", ">", "=", "-", "_" ]
+const charLower = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const charUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const charSpec = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", ".", "`", "~", "|", "<", ">", "=", "-", "_"];
+const charNumber = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+var charList = [];
+
+//console.log(charList);
 
 if (userOptions.lowerCase){
-  var charLower = charList.slice(26,52);
-  console.log(charLower);
+   // var charList = charList.concat(charLower);
+   charLower.forEach(element => charList.push(element));
+  //charList.push(charLower);
+  console.log(charList);
 }
+
 
 if(userOptions.upperCase) {
-  var charUpper = charList.slice(0,26);
-  console.log(charUpper);
+    //charlist = charList.concat(charUpper);
+    charUpper.forEach(element => charList.push(element));
+    //charList.push(charUpper);
+  console.log(charList);
 }
 
+
 if(userOptions.specialChar){
-  var charSpec = charList.slice(62,85);
-  console.log(charSpec);
+    //charlist = charList.concat(charSpec);
+    charSpec.forEach(element => charList.push(element));
+   // charList.push(charSpec);
+  console.log(charList);
 }
 
 if(userOptions.numbers){
-  var charNumber = charList.slice(52,62);
-  console.log(charNumber);
+    //charlist = charList.concat(charNumber);
+    charNumber.forEach(element => charList.push(element));
+   // charList.push(charNumber);
+  console.log(charList);
 }
 
 
-var charList = [charLower + charUpper + charSpec + charNumber];
-
-console.log(charList);
-
-var password = ''
-  for (var i=0; i < userOptions.numberOfChars; userOptions.numberOfChars++) {
-   password+= charList Math.random()* userOptions.numberOfChars;
-}
-
-return(password);
+  var passwordMake = []
+  for (var i=0; i < userOptions.numberOfChars; i++) {
+   //create an index that represents a random number against our character array
+   var randomIndex = Math.floor(Math.random() * charList.length);
+   //console.log(charList[randomIndex]);
+   // use the random index to ref back to the index position of charchters in the char list
+   
+   passwordMake.push(charList[randomIndex]);
+   
+  }
+//use the join method to turn the password from an array into a string
+ return(passwordMake.join(''));
 
 }
 
